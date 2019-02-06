a2903b712ba0e51b0265e3e5a144cea2b5bbfd77
Cassandra Corrodi <cassandra.corrodi.external@atos.net>
2019-02-06 11:10:39 +0100

    controller for all views created

--- Timetable/App_Start/RouteConfig.cs
+++ Timetable/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Timetable
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Timetable", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
