f2e96342df7ee7b0903bb0c4f36c183fbaed1867
Cassandra Corrodi <cassandra.corrodi.external@atos.net>
2019-02-06 17:20:12 +0100

    Lesson Service implemented (with interface)

--- Timetable.BusinessLogic/Lesson/ILessonService.cs
+++ Timetable.BusinessLogic/Lesson/ILessonService.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using Timetable.Domain.Model;
 
 namespace Timetable.BusinessLogic.Lesson
 {
-    class ILessonService
+    public interface ILessonService
     {
+        IEnumerable<Domain.Model.Lesson> GetLessons();
+        void CreateLesson(Domain.Model.Lesson lesson);
     }
 }
--- Timetable.BusinessLogic/Lesson/LessonService.cs
+++ Timetable.BusinessLogic/Lesson/LessonService.cs
@@ -1,12 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Timetable.Domain.Services;
 
 namespace Timetable.BusinessLogic.Lesson
 {
-    class LessonService
+    public class LessonService : ILessonService
     {
+        private readonly ILessonRepository _lessonRepository;
+
+        public LessonService(ILessonRepository lessonRepository)
+        {
+            _lessonRepository = lessonRepository;
+        }
+
+
+        public IEnumerable<Domain.Model.Lesson> GetLessons()
+        {
+            return _lessonRepository.GetAll().OrderBy(lesson => lesson.LessonName);
+        }
+
+        public void CreateLesson(Domain.Model.Lesson lesson)
+        {
+            _lessonRepository.CreateLesson(lesson);
+        }
     }
 }
