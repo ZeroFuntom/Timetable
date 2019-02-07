60d47c3afa8b3f3118df1cfbd6711f00ecd08dd2
Cassandra Corrodi <cassandra.corrodi.external@atos.net>
2019-02-07 10:23:08 +0100

    Timetable Context implemented

---
+++ Timetable.Domain/Model/ITimetableContext.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.Entity;
+
+namespace Timetable.Domain.Model
+{
+    public interface ITimetableContext
+    {
+        DbSet<Lesson> Lessons { get; set; }
+        int SaveChanges();
+    }
+}
---
+++ Timetable.Domain/Model/TimetableContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Data.Entity;
+using System.Data.Entity.SqlServer;
+
+namespace Timetable.Domain.Model
+{
+    public partial class TimetableEntities : ITimetableContext
+    {
+        public Type ProviderType { get; } = typeof(SqlProviderServices);
+    }
+}
